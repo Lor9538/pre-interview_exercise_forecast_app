38c7a33ffc6bc971d9693bca46d4d3bf8b0ddbb6
Lauren Southall <142028840+Lor9538@users.noreply.github.com>
2023-11-22 14:29:53 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# Define API base URL as a constant
+API_BASE_URL = "https://durvp011gk.execute-api.eu-west-1.amazonaws.com/v1/api/forecasts"
+
+# API Key for accessing the weather forecast API
+WEATHER_API_KEY = '8xc71bYjFg7MCXo74XhVJaD3d0XW3dto68Rrjgvx'
+
+# Default file extension for saving weather data
+JSON_FILE_EXTENSION = 'json'
